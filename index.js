39dfa5a17c92c7b1e943127d7a8d9569a8045c0f
luca pomer <ucapomer@gmail.com>
2019-12-10 11:24:37 +0100

    first commit!

---
+++ index.js
@@ -0,0 +1,23 @@
+const simpleGit = require('simple-git');
+let url = 'https://github.com/LucaPomer/workOverleaf.git';
+/**require('simple-git')()
+    .add('./*')
+    .commit("first commit!")
+    .addRemote('origin', url)
+    .push(['-u', 'origin', 'master'], () => console.log('done'));
+
+
+simpleGit()
+    .listRemote(['--get-url'], (err, data) => {
+        if (!err) {
+            console.log('Remote url for repository at ' + __dirname + ':');
+            console.log(data);
+        }
+    });**/
+
+simpleGit()
+    .init()
+    .add('./*')
+    .commit("first commit!")
+    .addRemote('origin', 'https://github.com/user/repo.git')
+    .push('origin', 'master');
